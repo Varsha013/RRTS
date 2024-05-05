f3a60cbf6bc64e5a13c49e3f3ba30afe590870df
Varsha013 <136807852+Varsha013@users.noreply.github.com>
2024-05-05 12:33:41 +0530

    Add files via upload

---
+++ RRTSProject/contactForm.php
@@ -0,0 +1,10 @@
+<div class="col-md-8"><!--start 1st column-->
+    <form action="" method="POST">
+      <input type="text" class="form-control" name="name" placeholder="Name"><br>
+      <input type="text" class="form-control" name="subject" placeholder="Subject"><br>
+      <input type="email" class="form-control" name="email" placeholder="Email"><br>
+      <textarea class="form-control" name="message" placeholder="How can we help you?" style="height:150px;"></textarea><br>
+      <input typt="submit" class="btn btn-primary" value="Send" name="Submit"><br>
+      <br>
+    </form>
+</div><!--end 1st column-->
